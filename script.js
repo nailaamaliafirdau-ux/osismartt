941880a8fe60cee8fd643a19a6970d89f1bf4bd7
nailaamaliafirdau-ux <nailaamaliafirdaus@gmail.com>
2025-10-30 11:20:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1.Pilih tombol berdasarkan ID-nya
+const tombolSapa =document.getElemenByld('sapaButton')
+//2. Tambahkan 'event listener' untuk aksi 'clicl'
+tombolSapa.addEventlistener('clicl',function(){
+  //3. Jalankan kode ini ketika tombol di-klik
+ alert('Halo! Terima kasih sudah berkunjung!);
+       });
